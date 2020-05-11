c7d7bb26937dd27ef7c877efa77bccc75cc2dce1
Uellington Damasceno <uellington99@gmail.com>
2020-05-11 02:45:06 -0300

    Code cleaning

--- src/games/pong/model/Ball.java
+++ src/games/pong/model/Ball.java
@@ -15,6 +15,7 @@ import util.Settings.Direction;
  */
 public class Ball extends Entity {
 
+    private Random drawer;
     private Rectangle2D rectangle;
     private double dx, dy, speed, angle;
     private Direction direction;
@@ -22,33 +23,31 @@ public class Ball extends Entity {
 
     public Ball(double x, double y, double width, double height) {
         super(x, y, width, height);
+        this.drawer = new Random();
         this.rectangle = new Rectangle2D(x, y, width, height);
-        this.drawInitialDirection();
+        this.drawDirection();
         this.collided = false;
         this.speed = 1.2;
     }
 
-    private void drawInitialDirection() {
-        Random drawer = new Random();
+    private void drawDirection() {
         boolean isRight = drawer.nextBoolean();
         boolean isTop = drawer.nextBoolean();
 
-        if (isRight && isTop) {
-            //Setor 3
-            angle = drawer.nextInt(60);
-            this.direction = Direction.EAST;
-        } else if (isRight && !isTop) {
-            //Sector 0
-            angle = drawer.nextInt(60) + 300;
-            this.direction = Direction.EAST;
-        } else if (!isRight && isTop) {
-            //sector 1
-            angle = drawer.nextInt(60) + 120;
-            this.direction = Direction.WEAST;
+        this.direction = (isRight) ? Direction.EAST : Direction.WEAST;
+
+        if (this.direction == Direction.EAST) {
+            if (isTop) {
+                angle = drawer.nextInt(60);
+            } else {
+                angle = drawer.nextInt(60) + 300;
+            }
         } else {
-            //sector 2
-            angle = drawer.nextInt(60) + 180;
-            this.direction = Direction.WEAST;
+            if (isTop) {
+                angle = drawer.nextInt(60) + 120;
+            } else {
+                angle = drawer.nextInt(60) + 180;
+            }
         }
         angle = Math.toRadians(angle);
         this.dx = Math.cos(angle);
@@ -76,31 +75,14 @@ public class Ball extends Entity {
         if (nextPositionY + this.height >= 250 || nextPositionY < 0) {
             this.dy *= -1;
         } else if (this.collided) {
-            Random drawer = new Random();
-            boolean isTop = drawer.nextBoolean();
-            if (this.direction == Direction.EAST) {
-                if (isTop) {
-                    angle = drawer.nextInt(60);
-                } else {
-                    angle = drawer.nextInt(60) + 300;
-                }
-            } else {
-                if (isTop) {
-                    angle = drawer.nextInt(60) + 120;
-                } else {
-                    angle = drawer.nextInt(60) + 180;
-                }
-            }
-            angle = Math.toRadians(angle);
-            this.dx = Math.cos(angle);
-            this.dy = Math.sin(angle);
+            this.drawDirection();
             this.collided = false;
         } else if (px >= 500 - this.width) {
+            this.drawDirection();
             this.setPosition(this.originPoint);
-            this.dx *= -1;
         } else if (px <= 0) {
+            this.drawDirection();
             this.setPosition(this.originPoint);
-            this.dx *= -1;
         }
 
         this.move(dx * speed, dy * speed);
